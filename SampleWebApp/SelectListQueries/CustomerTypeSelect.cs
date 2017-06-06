0d57e8554aa1a453407dbe116c1eeaca9e1747e3
Adam O'Neil <adamosoftware@gmail.com>
2017-06-06 15:11:49 -0400

    added GetMissingItem override on CustomerTypeSelect

--- SampleWebApp/SelectListQueries/CustomerTypeSelect.cs
+++ SampleWebApp/SelectListQueries/CustomerTypeSelect.cs
@@ -1,4 +1,7 @@
-﻿using Postulate.Mvc;
+﻿using System.Data;
+using System.Web.Mvc;
+using Postulate.Mvc;
+using Sample.Models;
 
 namespace SampleWebApp.SelectListQueries
 {
@@ -11,6 +14,15 @@ namespace SampleWebApp.SelectListQueries
             ORDER BY [Name]", 
             "CustomerTypeList", "TypeId")
         {
-        }        
+        }
+
+        /// <summary>
+        /// If an inactive customer type was used, this will get it in order to populate the dropdown -- otherwise, the field will look empty in the view
+        /// </summary>
+        public override SelectListItem GetMissingItem(IDbConnection connection, object id)
+        {
+            var item = new DemoDb2().Find<CustomerType>((int)id);
+            return new SelectListItem() { Value = item.Id.ToString(), Text = item.Name };
+        }
     }
 }
