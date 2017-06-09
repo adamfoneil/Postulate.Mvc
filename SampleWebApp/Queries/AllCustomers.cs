0f87425f472011c5063b43e74569698aff6632bc
Adam O'Neil <adamosoftware@gmail.com>
2017-06-08 20:32:57 -0400

    got Customer/Index working

--- SampleWebApp/Queries/AllCustomers.cs
+++ SampleWebApp/Queries/AllCustomers.cs
@@ -32,12 +32,12 @@ namespace SampleWebApp.Queries
                 INNER JOIN [dbo].[CustomerType] AS [ct] ON [c].[TypeId]=[ct].[Id]
                 INNER JOIN [dbo].[Region] AS [r] ON [c].[RegionId]=[r].[Id]
             WHERE
-                [c].[OrganizationId]=@orgId
+                [c].[OrganizationId]=@orgId {andWhere}
             ORDER BY
                 [LastName], [FirstName]")
         {
         }
-        
+                
         public int OrgId { get; set; }
 
         [Where("[LastName] LIKE '%'+@lastName+'%'")]
