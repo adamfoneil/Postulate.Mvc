e6fa32fe8a5e8bfaf42f454a2518d514020211fb
Adam O'Neil <adamosoftware@gmail.com>
2017-10-20 10:45:09 -0400

    version 1.0.2 IPrincipalExtensions with LoadUserData method

---
+++ Postulate.Mvc/Extensions/IPrincipalExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Postulate.Mvc.Abstract;
+using System.Security.Principal;
+using System.Web;
+
+namespace Postulate.Mvc.Extensions
+{
+    public static class IPrincipalExtensions
+    {
+        public static T LoadUserData<T>(this IPrincipal user, HttpServerUtilityBase server) where T : UserData, new()
+        {
+            return UserData.Load<T>(server, user.Identity.Name);
+        }
+    }
+}
