4f70a69dc9ee1575d759b0a71774be32561f5340
Adam O'Neil <adamosoftware@gmail.com>
2017-10-26 08:39:57 -0400

    version 1.0.6 YesNoDropdown will nullable bound value

--- Postulate.Mvc/Extensions/HtmlHelpers.cs
+++ Postulate.Mvc/Extensions/HtmlHelpers.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
 
 namespace Postulate.Mvc.Extensions
 {
@@ -30,24 +31,34 @@ namespace Postulate.Mvc.Extensions
             return hidden;
         }
 
-        public static MvcHtmlString YesNoDropdown(this HtmlHelper html, string fieldName, bool value, string yesText = "Yes", string noText = "No", object htmlAttributes = null)
+        public static MvcHtmlString YesNoDropdown(this HtmlHelper html, string fieldName, bool? value, string yesText = "Yes", string noText = "No", object htmlAttributes = null)
         {
             TagBuilder select = new TagBuilder("select");
             select.MergeAttribute("name", fieldName);
             select.MergeAttribute("id", fieldName);
             if (htmlAttributes != null) select.MergeAttributes(HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes));
 
-            select.InnerHtml = YesNoDropdownOption(true, value, yesText) + YesNoDropdownOption(false, value, noText);
+            select.InnerHtml = 
+                BlankOption() + 
+                YesNoDropdownOption(true, value, yesText) + 
+                YesNoDropdownOption(false, value, noText);
 
             return MvcHtmlString.Create(select.ToString(TagRenderMode.Normal));
         }
 
-        private static string YesNoDropdownOption(bool optionValue, bool boundValue, string text)
+        private static string BlankOption()
+        {
+            TagBuilder option = new TagBuilder("option");
+            option.SetInnerText("(select)");
+            return option.ToString(TagRenderMode.Normal);
+        }
+
+        private static string YesNoDropdownOption(bool optionValue, bool? boundValue, string text)
         {
             TagBuilder option = new TagBuilder("option");
             option.MergeAttribute("value", optionValue.ToString().ToLower());
             option.SetInnerText(text);
-            if (boundValue == optionValue) option.MergeAttribute("selected", "true");
+            if (boundValue?.Equals(optionValue) ?? false) option.MergeAttribute("selected", "true");
             return option.ToString(TagRenderMode.Normal);
         }
     }
