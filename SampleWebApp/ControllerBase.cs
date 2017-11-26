f190beada04be18b6ae6b5cbffca9b9cb39eb569
Adam O'Neil <adamosoftware@gmail.com>
2017-11-26 14:02:57 -0500

    new home page and error page

--- SampleWebApp/ControllerBase.cs
+++ SampleWebApp/ControllerBase.cs
@@ -8,8 +8,13 @@ namespace SampleWebApp
 {
     public class ControllerBase : ProfileControllerBase<DemoDb, int, UserProfile>
     {
+        public ControllerBase()
+        {
+            ExceptionView = "~/Views/Shared/Error.chstml";
+        }
+
         protected override ActionResult ProfileUpdateRedirect => RedirectToAction("Index", "Manage", new { message = ManageMessageId.ProfileMissing });
 
-        protected override Func<UserProfile, bool> ProfileRule => (record) => record.CurrentOrgId != 0;
+        protected override Func<UserProfile, bool> ProfileRule => (record) => record.CurrentOrgId != 0;        
     }
 }
