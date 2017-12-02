a50dfc531418e3c47ee9b8f338b3f07ac4f47adc
Adam O'Neil <adamosoftware@gmail.com>
2017-12-02 17:44:22 -0500

    SuccessMessage attribute and SaveMessage helper

---
+++ Postulate.Mvc/Attributes/SuccessMessage.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+
+namespace Postulate.Mvc.Attributes
+{
+    /// <summary>
+    /// Apply this to controller actions to add a message on successful completion of an operation.
+    /// Use Html.SaveMessage(TempData) in Razor views to access the bootstrap-formatted message
+    /// </summary>
+    public class SuccessMessage : ActionFilterAttribute
+    {
+        private readonly string _message;
+
+        public SuccessMessage(string message)
+        {
+            _message = message;
+        }
+
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            base.OnResultExecuted(filterContext);
+
+            const string key = "success";
+            var tempData = filterContext.Controller.TempData;
+            if (tempData.ContainsKey(key)) tempData.Remove(key);
+            tempData.Add(key, _message);
+        }
+    }
+}
