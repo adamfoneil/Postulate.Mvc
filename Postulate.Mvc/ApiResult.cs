5daf577555e77726e222f5d649e76d0d1185daa3
Adam O'Neil <adamosoftware@gmail.com>
2018-03-07 20:24:38 -0500

    version 1.1.5 added ApiResult

---
+++ Postulate.Mvc/ApiResult.cs
@@ -0,0 +1,16 @@
+﻿namespace Postulate.Mvc
+{
+	public class ApiResult<T>
+	{
+		private readonly T _data;
+
+		public ApiResult(T data)
+		{
+			_data = data;
+		}
+
+		public bool IsSuccessful { get; set; }
+		public string Message { get; set; }
+		public T Data { get { return _data; } }
+	}
+}
